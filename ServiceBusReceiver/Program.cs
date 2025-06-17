11226f94758dd17c455e0a1ccb5f2b3b8c9761db
unknown <nghia143318@gmail.com>
2025-06-17 16:05:19 +0700

    today

---
+++ ServiceBusReceiver/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Azure.Messaging.ServiceBus;
+using System.Text.Json;
+
+//string connectionString = "Endpoint=sb://mentossrvbus.servicebus.windows.net/;SharedAccessKeyName=queuepolicy;SharedAccessKey=YGl/cJ+F7sejJKEHfCZ0SHEZOBGd4Xxvs+ASbNPBePg=;EntityPath=firstqueue";
+string queueName = "firstqueue";
+// service bus client
+ServiceBusClient serviceBusClient = new ServiceBusClient(connectionString);
+
+// service bus receiver
+ServiceBusReceiver serviceBusReceiver = serviceBusClient.CreateReceiver(queueName);
+
+// receive & read message
+var message = await serviceBusReceiver.ReceiveMessageAsync();
+string messageInJson = message.Body.ToString();
+
+// convert message into Employee object using Deserialization
+Employee employeeDetail = JsonSerializer.Deserialize<Employee>(messageInJson);
+
+Console.WriteLine(employeeDetail.FirstName);
+Console.WriteLine("msg received!");
+
+class Employee
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Salary { get; set; }
+}
