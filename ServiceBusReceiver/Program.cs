7155ff8daf20f6cae6a3c29d308b4d064f7e5313
MOD Administrator <admin@MngEnvMCAP732343.onmicrosoft.com>
2025-06-17 16:50:27 +0700

    updated

--- ServiceBusReceiver/Program.cs
+++ ServiceBusReceiver/Program.cs
@@ -1,8 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Azure.Messaging.ServiceBus;
+using Microsoft.Azure.Amqp;
+using Microsoft.Azure.Amqp.Framing;
+using System.Reflection;
 using System.Text.Json;
 
-//string connectionString = "Endpoint=sb://mentossrvbus.servicebus.windows.net/;SharedAccessKeyName=queuepolicy;SharedAccessKey=YGl/cJ+F7sejJKEHfCZ0SHEZOBGd4Xxvs+ASbNPBePg=;EntityPath=firstqueue";
+string connectionString = "Endpoint=sb://mentossrvbus.servicebus.windows.net/;SharedAccessKeyName=queuepolicy;SharedAccessKey=YGl/cJ+F7sejJKEHfCZ0SHEZOBGd4Xxvs+ASbNPBePg=;EntityPath=firstqueue";
 string queueName = "firstqueue";
 // service bus client
 ServiceBusClient serviceBusClient = new ServiceBusClient(connectionString);
@@ -10,14 +13,20 @@ ServiceBusClient serviceBusClient = new ServiceBusClient(connectionString);
 // service bus receiver
 ServiceBusReceiver serviceBusReceiver = serviceBusClient.CreateReceiver(queueName);
 
-// receive & read message
-var message = await serviceBusReceiver.ReceiveMessageAsync();
-string messageInJson = message.Body.ToString();
+//// receive & read message
+//var message = await serviceBusReceiver.ReceiveMessageAsync();
+//string messageInJson = message.Body.ToString();
 
-// convert message into Employee object using Deserialization
-Employee employeeDetail = JsonSerializer.Deserialize<Employee>(messageInJson);
+//// convert message into Employee object using Deserialization
+//Employee employeeDetail = JsonSerializer.Deserialize<Employee>(messageInJson);
 
-Console.WriteLine(employeeDetail.FirstName);
+//Console.WriteLine(employeeDetail.FirstName);
+
+// delete message with sequence number
+//int sequenceNumber = "";
+//ServiceBusReceivedMessage deferredMessage = await serviceBusReceiver.ReceiveDeferredMessageAsync(sequenceNumber);
+//await serviceBusReceiver.CompleteMessageAsync(deferredMessage);
+
 Console.WriteLine("msg received!");
 
 class Employee
